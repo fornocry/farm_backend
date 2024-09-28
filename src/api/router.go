74f7d15dba0e320084c0b039a6485c0ca6b494e7
user <asdasd@gmail.com>
2024-09-28 23:40:39 +0300

    gzip

--- src/api/router.go
+++ src/api/router.go
@@ -3,6 +3,7 @@ package api
 import (
 	"crazyfarmbackend/config/di"
 	"crazyfarmbackend/src/api/routers"
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,7 @@ func Init(init *di.Initialization) *gin.Engine {
 	router.Use(init.MiddlewareService.CorsMiddleware())
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+	router.Use(gzip.Gzip(gzip.BestSpeed))
 	{
 		routers.SetupMainGroup(router, init)
 	}
